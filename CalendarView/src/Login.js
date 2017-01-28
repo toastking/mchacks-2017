18c372c71d0656c132a9a2db0d3f5d4ecfeeb11d
Henry <henry.chin@stonybrook.edu>
2017-01-28 18:11:00 -0500

    Refactored Login into more manageable pieces

--- CalendarView/src/Login.js
+++ CalendarView/src/Login.js
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+import './Login.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {signup: false}
+    this.changeToLogin = this.changeToLogin.bind(this)
+    this.changeToSignup = this.changeToSignup.bind(this)
+  }
+
+  renderForm() {
+    if (this.state.signup) {
+        return <SignupForm/>
+    }
+    else {
+        return <LoginForm/>
+    }
+  }
+
+  changeToLogin() {
+    this.setState({signup: false});
+  }
+
+  changeToSignup() {
+    this.setState({signup: true});
+  }
+
   render() {
     return (
         <div className="container">
@@ -10,54 +37,17 @@ class Login extends Component {
                     <div className="panel-body">
                     <div className="row">
                         <div className="col-lg-12">
-                        <form id="login-form" action="#" method="post" role="form" styles="display: block;">
-                            <h2>LOGIN</h2>
-                            <div className="form-group">
-                                <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Name" value=""/>
-                            </div>
-                            <div className="form-group">
-                                <input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password"/>
-                            </div>
-                            <div className="col-xs-6 form-group pull-left checkbox">
-                                <input id="checkbox1" type="checkbox" name="remember"/>
-                                <label for="checkbox1">Remember Me</label>   
-                            </div>
-                            <div className="col-xs-6 form-group pull-right">     
-                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" className="form-control btn btn-login" value="Log In"/>
-                            </div>
-                        </form>
-                        <form id="signup-form" action="#" method="post" role="form" styles="display: none;">
-                            <h2>Sign Up</h2>
-                            <div className="form-group">
-                                <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Name" value=""/>
-                            </div>
-                            <div className="form-group">
-                                <input type="email" name="email" id="email" tabindex="1" className="form-control" placeholder="Email Address" value=""/>
-                            </div>
-                            <div className="form-group">
-                                <input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="password" name="confirm-password" id="confirm-password" tabindex="2" className="form-control" placeholder="Confirm Password"/>
-                            </div>
-                            <div className="form-group">
-                                <div className="row">
-                                <div className="col-sm-6 col-sm-offset-3">
-                                    <input type="submit" name="signup-submit" id="signup-submit" tabindex="4" className="form-control btn btn-signup" value="Sign Up"/>
-                                </div>
-                                </div>
-                            </div>
-                        </form>
+                        {this.renderForm()}
                         </div>
                     </div>
                     </div>
                     <div className="panel-heading">
                     <div className="row">
                         <div className="col-xs-6 tabs">
-                        <a href="#" className="active" id="login-form-link"><div className="login">LOGIN</div></a>
+                        <a href="#" className="active" id="login-form-link" onClick={this.changeToLogin}><div className="login">LOGIN</div></a>
                         </div>
                         <div className="col-xs-6 tabs">
-                        <a href="#" id="signup-form-link"><div className="signup">SIGN UP</div></a>
+                        <a href="#" id="signup-form-link" onClick={this.changeToSignup} ><div className="signup">SIGN UP</div></a>
                         </div>
                     </div>
                     </div>
@@ -67,6 +57,7 @@ class Login extends Component {
         </div>
     )
   }
+
 };
 
 export default Login;

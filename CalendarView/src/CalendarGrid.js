100cf50b7cd8cee220b799d1e0f7fe3859dbaec6
Henry <henry.chin@stonybrook.edu>
2017-01-28 17:05:19 -0500

    Barebones working calendar page

---
+++ CalendarView/src/CalendarGrid.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import CalendarRow from './CalendarRow'
+
+class CalendarGrid extends Component {
+    splitArray(size) {
+        var a = this.props.days;
+        var arrays = [];
+
+        while (a.length > 0)
+            arrays.push(a.splice(0, size));
+
+        return arrays
+    }
+
+    renderWeeks() {
+        return this.splitArray(7).map(dayChunk => (
+            <CalendarRow key={dayChunk} days={dayChunk} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="CalendarGrid">
+                {this.renderWeeks()}
+            </div>
+        );
+    }
+
+}
+
+export default CalendarGrid;

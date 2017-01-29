bd31dacb26b1fc0b96799740cb846a231474f2cd
Matt Del Signore <mattdelsig@me.com>
2017-01-28 22:19:50 -0500

    fixed some jsx issues

--- CalendarView/src/SignupForm.js
+++ CalendarView/src/SignupForm.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import './Login.css';
 import $ from 'jquery';
 import jQuery from 'jquery';
-import {firebase} from 'Firebase';
+import * as firebase from 'firebase';
+
 // export for others scripts to use
 window.$ = $;
 window.jQuery = jQuery;
@@ -10,10 +11,15 @@ window.jQuery = jQuery;
 
 class SignupForm extends Component {
     createAccount(){
-        var ref = new Firebase("https://emo-journal.firebaseio.com");
-        var authClient = new FirebaseAuthClient(ref, function(error, user) {
 
-        });
+      var email = $("#email").text,
+      password = $("#password").text;
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
     }
 
   render() {
@@ -21,21 +27,21 @@ class SignupForm extends Component {
         <form id="signup-form" action="#"  role="form">
             <h2>Sign Up</h2>
             <div className="form-group">
-                <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Name" value=""/>
+                <input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Name" value=""/>
             </div>
             <div className="form-group">
-                <input type="email" name="email" id="email" tabindex="1" className="form-control" placeholder="Email Address" value=""/>
+                <input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" value=""/>
             </div>
             <div className="form-group">
-                <input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password"/>
+                <input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password"/>
             </div>
             <div className="form-group">
-                <input type="password" name="confirm-password" id="confirm-password" tabindex="2" className="form-control" placeholder="Confirm Password"/>
+                <input type="password" name="confirm-password" id="confirm-password" tabIndex="2" className="form-control" placeholder="Confirm Password"/>
             </div>
             <div className="form-group">
                 <div className="row">
                 <div className="col-sm-6 col-sm-offset-3">
-                    <input  type="submit" onClick ={this.createAccount} name="signup-submit" id="signup-submit" tabindex="4" className="form-control btn btn-signup" value="Sign Up"/>
+                    <input  type="submit" onClick ={this.createAccount} name="signup-submit" id="signup-submit" tabIndex="4" className="form-control btn btn-signup" value="Sign Up"/>
                 </div>
                 </div>
             </div>

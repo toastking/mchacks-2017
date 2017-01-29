5bc42c6fa5b483a581871a4b4dd29ceb590df47c
kwakuoowusu <kwakuoowusu@gmail.com>
2017-01-28 21:44:28 -0500

    trying to get firebase to install

--- CalendarView/src/SignupForm.js
+++ CalendarView/src/SignupForm.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 import './Login.css';
+import $ from 'jquery';
+import jQuery from 'jquery';
+import {firebase} from 'Firebase';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
 
 class SignupForm extends Component {
+    createAccount(){
+        var ref = new Firebase("https://emo-journal.firebaseio.com");
+        var authClient = new FirebaseAuthClient(ref, function(error, user) {
+
+        });
+    }
+
   render() {
     return (
-        <form id="signup-form" action="#" method="post" role="form">
+        <form id="signup-form" action="#"  role="form">
             <h2>Sign Up</h2>
             <div className="form-group">
                 <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Name" value=""/>
@@ -21,7 +35,7 @@ class SignupForm extends Component {
             <div className="form-group">
                 <div className="row">
                 <div className="col-sm-6 col-sm-offset-3">
-                    <input type="submit" name="signup-submit" id="signup-submit" tabindex="4" className="form-control btn btn-signup" value="Sign Up"/>
+                    <input  type="submit" onClick ={this.createAccount} name="signup-submit" id="signup-submit" tabindex="4" className="form-control btn btn-signup" value="Sign Up"/>
                 </div>
                 </div>
             </div>
@@ -29,5 +43,6 @@ class SignupForm extends Component {
     )
   }
 };
+
 
 export default SignupForm;
